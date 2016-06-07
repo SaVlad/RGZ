1b913f8cf01b326a670a0d77eb424bb14e00b3ac
Savchenko Vladislav <don.yari@yandex.ru>
2016-06-07 08:53:14 +0600

    Наполнение заголовочных файлов

--- RGZ/polynom.h
+++ RGZ/polynom.h
@@ -4,6 +4,54 @@
 #include "stdafx.h"
 
 class Polynom{
+private:
+	int length;
+	float*values;
+public:
+	Polynom();
+	Polynom(int, float*);
+	Polynom(Polynom&);
+
+	~Polynom();
+
+	void Print();
+
+	Polynom operator+(Polynom);
+	Polynom operator-(Polynom);
+	Polynom operator*(Polynom);
+	Polynom operator/(Polynom);
+	friend Polynom operator+(float, Polynom);
+	friend Polynom operator-(float, Polynom);
+	friend Polynom operator*(float, Polynom);
+	friend Polynom operator/(float, Polynom);
+	Polynom operator+(float);
+	Polynom operator-(float);
+	Polynom operator*(float);
+	Polynom operator/(float);
+
+	void operator+=(Polynom);
+	void operator-=(Polynom);
+	void operator*=(Polynom);
+	void operator/=(Polynom);
+	void operator+=(float);
+	void operator-=(float);
+	void operator*=(float);
+	void operator/=(float);
+
+	Polynom operator++();
+	Polynom operator++(int);
+	Polynom operator--();
+	Polynom operator--(int);
+	Polynom operator-();
+	Polynom operator+();
+
+	bool operator<(Polynom);
+	bool operator>(Polynom);
+	bool operator==(Polynom);
+	bool operator!=(Polynom);
+
+	float operator[](int);
+	float operator()(int);
 };
 
 #endif
--- RGZ/testing.h
+++ RGZ/testing.h
@@ -4,6 +4,52 @@
 #include "stdafx.h"
 
 class Testing{
+public:
+	Testing();
+
+	void ConstructorDef();
+	void ConstructorPar();
+	void ConstructorClo();
+
+	void Print();
+
+	void ppAdd();
+	void ppSub();
+	void ppMul();
+	void ppDiv();
+	void fpAdd();
+	void fpSub();
+	void fpMul();
+	void fpDiv();
+	void pfAdd();
+	void pfSub();
+	void pfMul();
+	void pfDiv();
+
+	void Addp();
+	void Subp();
+	void Mulp();
+	void Divp();
+	void Addf();
+	void Subf();
+	void Mulf();
+	void Divf();
+
+	void incPost();
+	void decPost();
+	void incPref();
+	void decPref();
+
+	void unPos();
+	void unNeg();
+
+	void Less();
+	void Greater();
+	void Equals();
+	void notEquals();
+
+	void sIndexer();
+	void rIndexer();
 };
 
 #endif
